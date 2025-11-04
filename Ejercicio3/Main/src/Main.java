6de9d24543c3eeb6293d81ce4a38bb4cf30ee19d
RST90 <rafa.st_@hotmail.com>
2025-11-04 18:23:11 +0100

    Hago ejercicio3
    New

---
+++ Ejercicio3/Main/src/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args){
+        int valor = 100;
+        valor +=50;
+        System.out.println(" Después de la suma 50: " +valor);
+        valor -=20;
+        System.out.println(" Después de restarle 20: " +valor);
+        valor *= 2;
+        System.out.println(" Después de multiplicarle 2: " +valor);
+        valor /= 5;
+        System.out.println(" Después de dividirle 5: " +valor);
+
+
+    }
+}
