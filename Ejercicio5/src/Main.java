67ec7d6d442e384364e0660eb0db58032b0d8480
RST90 <rafa.st_@hotmail.com>
2025-11-04 19:36:09 +0100

    update
    termino ejercicio

--- Ejercicio5/src/Main.java
+++ Ejercicio5/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String [] args){
         Operador operadoresLogicos = new Operador();
+        Scanner lecturaTeclado = new Scanner(System.in);
+        System.out.println("¿Qué edad tienes?" );
+        int edad = lecturaTeclado.nextInt();
+        System.out.println("¿Tienes carnet de conducir?" );
+        boolean carnetConducir = lecturaTeclado.nextBoolean();
+        operadoresLogicos.alquilerCoche(edad, carnetConducir);
     }
 }
--- Ejercicio5/src/Operador.java
+++ Ejercicio5/src/Operador.java
@@ -1,3 +1,6 @@
 public class Operador{
-    public void alquilerCoche( int edad)
+    public void alquilerCoche(int edad, boolean carnetConducir) {
+        boolean resultado =  (edad>= 21) && carnetConducir;
+        System.out.println("La posibilidad de que alquiles un coche es = " +resultado);
+    }
 }
