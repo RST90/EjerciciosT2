812b3c8df6ddde684761eb5c9afacce1dc19e18e
RST90 <rafa.st_@hotmail.com>
2025-11-04 18:06:38 +0100

    Hago ejercicio 2

---
+++ Ejercicio2/untitled/src/Comparacion.java
@@ -0,0 +1,11 @@
+public class Comparacion{
+    public void comparacionesMatematicas(int valorUno, int valorDos){
+        System.out.println("¿" +valorUno+ " es mayor que " +valorDos + "? " + (valorUno>valorDos));
+        System.out.println("¿" +valorUno+ " es menor que " +valorDos + "? " + (valorUno<valorDos));
+        System.out.println("¿" +valorUno+ " es igual que " +valorDos + "? " + (valorUno==valorDos));
+        System.out.println("¿" +valorUno+ " es diferente que " +valorDos + "? " + (valorUno!=valorDos));
+        System.out.println("¿" +valorUno+ " es mayor o igual que " +valorDos + "? " + (valorUno>=valorDos));
+        System.out.println("¿" +valorUno+ " es menor o igual que " +valorDos + "? " + (valorUno<=valorDos));
+
+    }
+}
---
+++ Ejercicio2/untitled/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Comparacion comparacionesAritmeticas = new Comparacion();
+        Scanner lecturaTeclado = new Scanner(System.in);
+        System.out.println("Indique primer número");
+        int valorUno = lecturaTeclado.nextInt();
+        System.out.println("Indique segundo número");
+        int valorDos = lecturaTeclado.nextInt();
+        comparacionesAritmeticas.comparacionesMatematicas(valorUno, valorDos);
+
+
+    }
+}
